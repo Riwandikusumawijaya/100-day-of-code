8dd548f2acfc856cd6ef29f2e4f20de723826e39
Riwandi Kusuma Wijaya <147130755+Riwandikusumawijaya@users.noreply.github.com>
2023-10-11 17:47:19 +0800

    Add files via upload

---
+++ HariKetiga.java
@@ -0,0 +1,28 @@
+
+package hari.ketiga;
+public class HariKetiga {
+    public static void main(String[] args) {
+        //manipulasi variable pada pemograman java
+        int umur;
+        umur = 81;
+        System.out.println(umur);
+        umur = 96;
+        System.out.println(umur);
+        umur = 12;
+        System.out.println(umur);
+        String nama = "riwan";
+        System.out.println(nama);
+        nama = "burhan";
+        System.out.println(nama);
+        String nama_depan, nama_belakang, nama_lengkap;
+        nama_depan = "riwandi";
+        nama_belakang = "kusuma wijaya";
+        System.out.println("Nama depan : ");
+        System.out.println("Nama Lengkap : ");
+        System.out.println(nama_depan);
+        System.out.println(nama_belakang);
+        nama_lengkap = nama_depan +" "+ nama_belakang;
+        System.out.println(nama_lengkap);
+    }
+    
+}
