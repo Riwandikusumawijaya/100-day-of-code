1dfceee1dbc2c15ed6b5ba9371df0136209134b6
Riwandi Kusuma Wijaya <147130755+Riwandikusumawijaya@users.noreply.github.com>
2023-10-12 19:21:39 +0800

    Add files via upload

---
+++ Hari_keKempat.java
@@ -0,0 +1,33 @@
+
+package hari_ke.kempat;
+public class Hari_keKempat {
+    public static void main(String[] args) {
+        System.out.println("Nama saya : Riwandi Kusuma Wijaya," 
+                         + " kelas : B23,"
+                         + " Jurusan : informatika, "
+                         + " Fakultas : Teknik, "
+                         + "Asal dari : Mamasa.");
+        System.out.println(8 + " tahun");
+        System.out.println("tahun " + 2023);
+        System.out.println("50,888888");
+        
+        if(true){
+            System.out.println("Belajar atau tidak lulus");
+        }else{
+            System.out.println("Pasrah saja");          
+        }
+       int angka = 2;
+       if(angka>2){
+           System.out.println("Salah");
+       }else if(angka>3){
+           System.out.println("Benar");
+       }else{
+           System.out.println("Santui");
+       }
+       
+       }
+        
+       
+    }
+    
+}
