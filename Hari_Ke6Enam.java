ac1ac853e6ef2c67d815681676379589b916f3d3
Riwandi Kusuma Wijaya <147130755+Riwandikusumawijaya@users.noreply.github.com>
2023-10-14 18:49:05 +0800

    Add files via upload

---
+++ Hari_Ke6Enam.java
@@ -0,0 +1,33 @@
+package hari_ke6.enam;
+import java.util.Scanner;
+public class Hari_Ke6Enam {
+    public static void main(String[] args) {
+     //Operator Arifmatika
+     /*double a = 5;
+     double b = 4;
+     double c = a - b;
+        System.out.println(c);*/
+     Scanner input = new Scanner (System.in);
+     int a, b, c;
+        System.out.print("masukkan a = ");
+        a = input.nextInt();
+        System.out.print("masukkan b = ");
+        b = input.nextInt();
+        c = a + b;
+        System.out.print(a + " + " + b + " = ");
+        System.out.println(c);
+        c = b - a;
+        System.out.println(b + " - " + a + " = " + c);
+        c = a * b;
+        System.out.println(a + " x " + b + " = " + c);
+        
+        
+        
+        
+       
+     
+      
+    
+        
+    }   
+}
