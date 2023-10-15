41ca34074969f29b87e2dd48e920dc5ed845652e
Riwandi Kusuma Wijaya <147130755+Riwandikusumawijaya@users.noreply.github.com>
2023-10-15 18:47:09 +0800

    Add files via upload

---
+++ Hari_ketuju(7).java
@@ -0,0 +1,24 @@
+
+package hari_ke7;
+public class Hari_ke7 {
+    public static void main(String[] args) {
+        System.out.println("Aku akan tetap mencintaimu,namun soal siapa yang ada dihatimu itu urusanmu");
+        System.out.println("Kenapa aku harus cemburu meliht kamu dekat dengan orang lain,padahal kita gak ada hubungan apa-apa");
+        System.out.println("Gini amat terjebak oleh rasa nyaman yang tidak akan mungkin bisa untuk dimiliki");
+        System.out.println("Dekat denganmu adalah sakit hati yang kusengaja ");
+        System.out.println("Aku cemburu?. Iya iyalah k0nt0l");
+        System.out.println("Pantas berubah ternyata masalalunya datang lagi wkwkw");
+        System.out.println("Lebih baik terjebak oleh ulti yin dari pada terjebak oleh rasa nyaman Namun hanya sebatas teman");
+        System.out.println("Jangan pernah menggunakan seseorang untuk melupakan seseorang");
+        System.out.println("Dia tau kamu menyukainya namun bukan kamu yang dia mau");
+        System.out.println("Dia senyum bukan berarti dia suka,jadi tidak usah berekspektasi terlalu tinggi");
+        System.out.println("Menyukai itu wajar yang gak wajar itu memaksa memiliki");
+        System.out.println("Titik tertinggi dalam dalam mencintai adalah merelakan");
+        System.out.println("Terpaksa melupakan seseorang yang sebenarnya masih ingin dimiliki");
+        System.out.println("Pusing bangat mikirin kamu takut ada yang ambil");
+        System.out.println("Selama masih ada foto dia di hp gua berarti gua belum sepenuhnya ngelupain dia");
+        System.out.println("Aku tau aku sedang dipermainkan tapi aku menikmatinya");
+    }}
+
+    
+
