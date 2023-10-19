8b59c1d137d62ea653c477c94a6f2d61ad5ef949
Riwandi Kusuma Wijaya <147130755+Riwandikusumawijaya@users.noreply.github.com>
2023-10-19 19:43:57 +0800

    Add files via upload

---
+++ Hari_ke11.java
@@ -0,0 +1,9 @@
+
+package hari_ke11;
+public class Hari_ke11 {
+    public static void main(String[] args) {
+        for (int i = 5; i <= 10; i++) {           
+            System.out.println("Tampilkan angka " + i);        
+        }  
+        }
+        }
