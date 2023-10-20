c6014eafc4c50b6e52d80eaecf15c73718076908
Riwandi Kusuma Wijaya <147130755+Riwandikusumawijaya@users.noreply.github.com>
2023-10-20 20:39:51 +0800

    Add files via upload

---
+++ HariKe12.java
@@ -0,0 +1,11 @@
+
+package hari.ke12;
+public class HariKe12 {
+    public static void main(String[] args) {
+        int umur = 19;
+        int angka = 9;
+        int nilai = 10;
+        System.out.println(umur * angka + nilai + angka);
+    }
+    
+}
