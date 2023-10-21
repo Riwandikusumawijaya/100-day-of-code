3f597790c7f9234b5f0c0e8f70bab9a6a5165890
Riwandi Kusuma Wijaya <147130755+Riwandikusumawijaya@users.noreply.github.com>
2023-10-21 21:27:29 +0800

    Add files via upload

---
+++ Hari_Ke13.java
@@ -0,0 +1,16 @@
+
+package javaapplication54;
+public class JavaApplication54 {
+    public static void main(String[] args) {
+    int angka  = 4;
+    int umur = 3;
+    double jumlah = 8.5;
+        System.out.println(angka + umur * jumlah);
+        for (int r = 9; r >= 4; r--) {
+            System.out.println(r);
+            
+        }
+    
+    }
+    
+}
