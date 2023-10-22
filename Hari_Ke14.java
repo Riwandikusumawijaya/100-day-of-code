f05b0df8b2e3e2cc312139459edf1f1da5c7b887
Riwandi Kusuma Wijaya <147130755+Riwandikusumawijaya@users.noreply.github.com>
2023-10-22 22:05:50 +0800

    Add files via upload

---
+++ Hari_Ke14.java
@@ -0,0 +1,9 @@
+package hari_ke14;
+public class Hari_Ke14 {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 11; i++) {
+            System.out.println(i+i*i);    
+        }
+    }
+    
+}
