bb41ba6c9e4736f8f47d14a475093eb0a61f29b9
Riwandi Kusuma Wijaya <147130755+Riwandikusumawijaya@users.noreply.github.com>
2023-10-23 21:10:03 +0800

    Add files via upload

---
+++ Hari_Ke15.java
@@ -0,0 +1,18 @@
+package hari_ke15;
+public class Hari_Ke15 {
+public static void main(String[] args) {
+int angka;
+angka = 10;
+    System.out.println(angka);
+angka = 58;
+    System.out.println(angka);
+    
+    int a, b;
+    a = 3;
+    b = 4;
+    a += b;
+    System.out.println(a);
+    b -= 1;
+    System.out.println(b);
+}
+}
