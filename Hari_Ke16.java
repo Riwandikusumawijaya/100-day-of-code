02f4ca6702bc8e41a303cf374ce1f2473c72ce9d
Riwandi Kusuma Wijaya <147130755+Riwandikusumawijaya@users.noreply.github.com>
2023-10-25 00:09:10 +0800

    Add files via upload

---
+++ Hari_Ke16.java
@@ -0,0 +1,15 @@
+
+package hari_ke16;
+
+public class Hari_Ke16 {
+    public static void main(String[] args) {
+       int angka,umur,tanggal;
+       angka = 4;
+       umur = 3;
+       tanggal = 2;
+        System.out.println(angka*umur*tanggal); 
+        
+       
+    }
+    
+}
