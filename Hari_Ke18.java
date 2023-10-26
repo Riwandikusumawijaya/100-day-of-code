9331911f0e2af3816da5aa76db1222f29f92a116
Riwandi Kusuma Wijaya <147130755+Riwandikusumawijaya@users.noreply.github.com>
2023-10-26 16:03:54 +0800

    Add files via upload

---
+++ Hari_Ke18.java
@@ -0,0 +1,16 @@
+
+package hari_ke18;
+public class Hari_Ke18 {   
+    public static void main(String[] args) {
+       //Operator perbandingan
+       int a = 10;
+       int b = 7;
+        System.out.println(a == b);
+        System.out.println(a != b);
+        System.out.println(a > b);
+        System.out.println(a >= b);
+        System.out.println(a < b);
+        System.out.println(a <= b);
+    }
+    
+}
