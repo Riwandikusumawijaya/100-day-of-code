778df2eea61f4842762eb263a1b11b5751e98bd0
Riwandi Kusuma Wijaya <147130755+Riwandikusumawijaya@users.noreply.github.com>
2023-10-27 17:21:41 +0800

    Add files via upload

---
+++ Hari_Ke19.java
@@ -0,0 +1,19 @@
+
+package hari_ke19;
+public class Hari_Ke19 {
+    public static void main(String[] args) {  
+        boolean lulus = false;
+        System.out.println(!lulus);
+        boolean a = true;
+        boolean b = false;
+        boolean c = a && b;
+        System.out.println(c);
+        boolean d = true;
+        boolean e = true;
+        boolean f = d || e;
+        System.out.println(f);
+        
+        
+    }
+    
+}
