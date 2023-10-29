7bead65fdf77b7167c89f7f87c18062ceb2169c1
Riwandi Kusuma Wijaya <147130755+Riwandikusumawijaya@users.noreply.github.com>
2023-10-29 19:30:49 +0800

    Add files via upload

---
+++ Hari_Ke21.java
@@ -0,0 +1,8 @@
+package hari_ke21;
+public class Hari_Ke21 {
+    public static void main(String[] args) {
+        for (int i = 15; i > 10; i--) {
+            System.out.println("Riwandi Kusuma Wijaya" );
+        }
+    }    
+}
