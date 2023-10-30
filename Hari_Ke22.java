6e190a7696c569332a99d4f7008aff9d197b1523
Riwandi Kusuma Wijaya <147130755+Riwandikusumawijaya@users.noreply.github.com>
2023-10-30 20:35:16 +0800

    Add files via upload

---
+++ Hari_Ke22.java
@@ -0,0 +1,13 @@
+package hari_ke22;
+public class Hari_Ke22 {
+    public static void main(String[] args) {
+     int a, b, c;
+     a = 3;
+     b = 9;
+     c = 9;
+        System.out.println(a<b);
+        System.out.println(c>b);
+        System.out.println(b==c);
+        System.out.println(b%a);
+    }   
+}
