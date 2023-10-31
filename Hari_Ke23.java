e93c92fc6e2e4553ab7a98df36bc6e3a93a13c82
Riwandi Kusuma Wijaya <147130755+Riwandikusumawijaya@users.noreply.github.com>
2023-10-31 17:40:59 +0800

    Add files via upload

---
+++ Hari_Ke23.java
@@ -0,0 +1,17 @@
+package hari_ke23;
+public class Hari_Ke23 {
+public static void main(String[] args) {
+int a = 19;
+     boolean b = true;
+     double c = 50;
+     float e = 2;
+     char d = 'L';
+     String f = "Riwandi Kusuma Wijaya";
+        System.out.println("Nama = " + f);
+        System.out.println("Umur = " + a + " Tahun");
+        System.out.println("Berat Badan = " + c + " Kg");
+        System.out.println("Gender = " + d); 
+        System.out.println("Nilait = " + e);
+            System.out.println("Data diatas = " + b);
+}    
+}
