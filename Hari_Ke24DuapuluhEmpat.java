f35377dd60719684aae756e269b8c5a87635d419
Riwandi Kusuma Wijaya <147130755+Riwandikusumawijaya@users.noreply.github.com>
2023-10-31 17:42:25 +0800

    Add files via upload

---
+++ Hari_Ke24DuapuluhEmpat.java
@@ -0,0 +1,22 @@
+
+package hari_ke24.duapuluh.empat;
+public class Hari_Ke24DuapuluhEmpat {
+    public static void main(String[] args) {
+      String nama = "Riwandi Kusuma Wijaya";
+      String nim = "D0223323";
+      String alamat = "Lembang";
+      double tinggiBadan = 1.5;
+      int beratBadan = 50;
+        System.out.println("Nama = " + nama);
+        System.out.println("Nim = " + nim);
+        System.out.println("Alamat = " + alamat);
+        System.out.println("Tinggi Badan = " + tinggiBadan + "m");
+        System.out.println("Berat Badan = " + beratBadan + " kg");
+        
+        
+      
+             
+      
+    }
+    
+}
