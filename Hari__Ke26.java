2773b7d51fd74bb982036755481a099ab77187a7
Riwandi Kusuma Wijaya <147130755+Riwandikusumawijaya@users.noreply.github.com>
2023-11-03 15:25:33 +0800

    Add files via upload

---
+++ Hari__Ke26.java
@@ -0,0 +1,19 @@
+package hari__ke26;
+import java.util.Scanner;
+public class Hari__Ke26 {
+    public static void main(String[] args) {
+     Scanner input = new Scanner(System.in);
+        int a, b, c, d;
+        System.out.print("Masukkan Nilai a = ");
+        a = input.nextInt();
+        System.out.print("Masukkan Nilai b = ");
+        b = input.nextInt();
+        System.out.print("Masukkan Nilai c = ");
+        c = input.nextInt();
+        System.out.print("Masukkan Nilai d = ");
+        d = input.nextInt();
+        
+        
+    }
+    
+}
