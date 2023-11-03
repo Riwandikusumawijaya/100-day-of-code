fef2c50b087774f11bded2afc3e48fe8215c766b
Riwandi Kusuma Wijaya <147130755+Riwandikusumawijaya@users.noreply.github.com>
2023-11-03 16:20:56 +0800

    Add files via upload

---
+++ Hari_Ke27.java
@@ -0,0 +1,34 @@
+package hari_ke27;
+import java.util.Scanner;
+public class Hari_Ke27 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner (System.in);
+        int angka;
+        int umur;
+        int jumlah;
+        System.out.print("Angka = ");
+        angka = input.nextInt();
+        System.out.print("Umur = ");
+        umur = input.nextInt();
+        System.out.print("Jumlah = ");
+        jumlah = input.nextInt();        
+            int a, b, c;
+            a = 3; 
+            b = 4;
+            c = 2;
+            System.out.println(a * b);
+            System.out.println(c * 4);
+            System.out.println(a * 8);
+            System.out.println(b += b);
+            for (int i = 1; i <= 20; i++) {
+                System.out.println(i);
+                
+            
+        }
+            
+            
+        
+        
+    }
+    
+}
