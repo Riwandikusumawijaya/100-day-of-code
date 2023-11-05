15f9ef111bb52e9b292f902f5800cd13d5e6cbbb
Riwandi Kusuma Wijaya <147130755+Riwandikusumawijaya@users.noreply.github.com>
2023-11-05 23:25:48 +0800

    Add files via upload

---
+++ Hari_Ke28.java
@@ -0,0 +1,10 @@
+package hari_ke28;
+public class Hari_Ke28 {
+    public static void main(String[] args) {
+      int umur = 20;
+        int usia = 19;
+        int hasil = umur * usia;
+        System.out.println(hasil);
+            
+    }    
+}
