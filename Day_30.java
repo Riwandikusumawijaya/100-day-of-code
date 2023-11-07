3314533906e23917a22f75cb526b98d2da26cccd
Riwandi Kusuma Wijaya <147130755+Riwandikusumawijaya@users.noreply.github.com>
2023-11-07 20:59:48 +0800

    Add files via upload

---
+++ Day_30.java
@@ -0,0 +1,40 @@
+package day_30;
+import java.util.Scanner;
+public class Day_30 {
+    public static void main(String[] args) {
+     Scanner input = new Scanner(System.in);
+     String nama;
+        System.out.print("Nama = ");
+        nama = input.nextLine();
+        String nim;
+        System.out.print("Nim = ");
+        nim = input.nextLine();
+        String prodi;
+        System.out.print("Prodi = ");
+        prodi = input.nextLine();
+        String kelas;
+        System.out.print("Kelas = ");
+        kelas = input.nextLine();
+        String asal;
+        System.out.print("Asal = ");
+        asal = input.nextLine();
+        int umur;
+        System.out.print("Umur = ");
+        umur = input.nextInt();
+        double tinggibadan;
+        System.out.print("Tinggi Badan = ");
+        tinggibadan = input.nextDouble();
+        double beratbadan;
+        System.out.print("Berat Badan = ");
+        beratbadan = input.nextDouble();
+        System.out.println("");
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+}
