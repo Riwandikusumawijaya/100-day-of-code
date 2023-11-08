5dfdad3d36cda00995da399ab047aca31b67d41d
Riwandi Kusuma Wijaya <147130755+Riwandikusumawijaya@users.noreply.github.com>
2023-11-08 22:00:12 +0800

    Add files via upload

---
+++ Hari_Ke31.java
@@ -0,0 +1,18 @@
+package hari_ke31;
+import java.util.Scanner;
+public class Hari_Ke31 {
+    public static void main(String[] args) {
+       Scanner input = new Scanner(System.in);
+       String gender;
+        System.out.print("Gender = ");
+        gender = input.nextLine();
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+            
+        }
+        
+        
+        
+        
+    }    
+}
