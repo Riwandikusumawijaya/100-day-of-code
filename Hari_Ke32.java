6527fd57525f7e13dfba5b4d1145f6ad35fb1130
Riwandi Kusuma Wijaya <147130755+Riwandikusumawijaya@users.noreply.github.com>
2023-11-09 20:42:29 +0800

    Add files via upload

---
+++ Hari_Ke32.java
@@ -0,0 +1,30 @@
+package hari_ke32;
+public class Hari_Ke32 {
+    public static void main(String[] args) {
+        //manipulasi variable pada pemograman java
+        System.out.print("Nama saya : Riwandi Kusuma Wijaya," 
+                        + "\n kelas : B23,"
+                        + "\n Jurusan : informatika, "
+                + "\n Fakultas : Teknik, "
+                         + "\n Asal dari : Mamasa.");
+
+       System.out.println("riwandi");
+        System.out.println("B23");
+        System.out.println("Teknik");
+        System.out.println("Informatika");
+        System.out.println("mamasa");
+        int m = 1;
+        
+        if(m == 1){
+            System.out.println("Belajar atau tidak lulus");
+        }
+        else{
+            System.out.println("Pasrah saja");    
+            
+        }
+
+    }
+    
+}
+
+   
