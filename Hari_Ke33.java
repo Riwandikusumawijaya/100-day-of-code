308ab18a11d3c900d0dc5d7515bc37f67f4d1cd3
Riwandi Kusuma Wijaya <147130755+Riwandikusumawijaya@users.noreply.github.com>
2023-11-10 21:22:52 +0800

    Add files via upload

---
+++ Hari_Ke33.java
@@ -0,0 +1,12 @@
+
+package hari_ke33;
+public class Hari_Ke33 {
+    public static void main(String[] args) {
+        for (int r = 1; r <= 3; r++) {
+            System.out.println("WANXZ");
+            
+        }
+            
+    }
+    
+}
