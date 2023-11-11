9f77db67730b112efa08dc07924bbfb06d1c8e5c
Riwandi Kusuma Wijaya <147130755+Riwandikusumawijaya@users.noreply.github.com>
2023-11-11 19:25:50 +0800

    Add files via upload

---
+++ Hari_Ke34.java
@@ -0,0 +1,13 @@
+package hari_ke34;
+public class Hari_Ke34 {
+    public static void main(String[] args) {
+        int umur = 19;
+        int angka = 10;
+ if(umur == angka){   
+     System.out.println("Belajar bahasa ingris");
+    } 
+ if (umur > angka){
+     System.out.println("Belajar bahasa indonesia");    
+ }
+    }
+}
