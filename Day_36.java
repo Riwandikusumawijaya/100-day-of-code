996b3e80d3a69cf0de7fbd3ee2d3d857f3f2dd31
Riwandi Kusuma Wijaya <147130755+Riwandikusumawijaya@users.noreply.github.com>
2023-11-13 21:32:47 +0800

    Add files via upload

---
+++ Day_36.java
@@ -0,0 +1,14 @@
+package day_36;
+public class Day_36 {
+    public static void main(String[] args) {
+        int umur = 9;
+        int angka = 6;
+        if(umur > angka){
+            System.out.println("tidak lulus");    
+        }
+        if(angka > umur){
+            System.out.println("lulus");    
+            
+        }
+    }    
+}
