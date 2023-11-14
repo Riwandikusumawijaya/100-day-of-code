f2d617d3f9c4d9e52c5d3ba44c26ec41979d2176
Riwandi Kusuma Wijaya <147130755+Riwandikusumawijaya@users.noreply.github.com>
2023-11-14 15:10:04 +0800

    Add files via upload

---
+++ Day_37.java
@@ -0,0 +1,34 @@
+package day_37;
+public class Day_37 {
+    public static void main(String[] args) {
+//operator Arifmatika
+        int a = 9;
+        int b = 4;
+        System.out.println(a + b);
+        System.out.println(a * b);
+        System.out.println(a - b);
+        System.out.println(a % b);
+//operator perulangan
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Mantap");
+//operator perbandingan 
+            int c = 6;
+            int d = 3;
+            System.out.println(c < d);
+            System.out.println(c > d);
+            System.out.println(c = d);
+            
+            int f = 8;
+            int e = 5;
+            if(f > e){
+                System.out.println("benar");
+                
+            }
+            if(f < e){
+                System.out.println("salah");                
+            }
+           
+        }
+    }
+
+}
