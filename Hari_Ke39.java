5b7da96e50d319cf8f11fbffca97be5dcdc0144c
Riwandi Kusuma Wijaya <147130755+Riwandikusumawijaya@users.noreply.github.com>
2023-11-16 23:17:58 +0800

    Add files via upload

---
+++ Hari_Ke39.java
@@ -0,0 +1,33 @@
+package hari_ke39;
+import java.util.Scanner;
+public class Hari_Ke39 {
+public static void main(String[] args) {
+Scanner sc = new Scanner(System.in);
+String nama,umur,tinggibadan,kelas,beratbadan,asal,
+nim,prodi;
+char gendeer;
+    System.out.print("Nama = ");
+    nama = sc.nextLine();
+    System.out.print("Nim = ");
+    nim = sc.nextLine();
+    System.out.print("Kelas = ");
+    kelas = sc.nextLine();
+    System.out.print("Prodi = ");
+    prodi = sc.nextLine();
+    System.out.print("Asal = ");
+    asal = sc.nextLine();
+    System.out.print("Tinggi Badan = ");
+    tinggibadan = sc.nextLine();
+    System.out.print("Berat Badan = ");
+    beratbadan = sc.nextLine();
+    System.out.print("Gender = ");
+    char gender = (char) sc.nextLong();
+    System.out.println("");
+   
+    
+    
+    
+    
+    
+}    
+}
