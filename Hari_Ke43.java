e97d6bd0b14c63b8bd194ee1f675f00b22590231
Riwandi Kusuma Wijaya <147130755+Riwandikusumawijaya@users.noreply.github.com>
2023-11-20 21:41:17 +0800

    Add files via upload

---
+++ Hari_Ke43.java
@@ -0,0 +1,19 @@
+package hari_ke.pkg43;
+import java.util.Scanner;
+public class Hari_Ke43 {
+
+    public static void main(String[] args) {
+    }
+
+    {
+        Scanner sc = new Scanner(System.in);
+        String jumlah, laki, perempuan;
+        System.out.print("Jumlah Siswa Kelas A = ");
+        jumlah = sc.nextLine();
+        System.out.print("Jumlah Laki-Laki = ");
+        laki = sc.nextLine();
+        System.out.print("Jumlah Perempuan = ");
+        perempuan = sc.nextLine();
+
+    }
+}
